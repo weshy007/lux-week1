33eed917cdd898e12577beb1b117f396592922ba
weshy <josephwaweru96@gmail.com>
2022-04-24 12:15:51 +0300

    prime number function

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## Lux Academy Week 1 Python Project
 - Two assesment tests made with python. One tests if a number is Prime or not, and the other if a number is in the number belongs to the Fibonacci sequence or not.
+
+## Features
+- The Prime number script takes in users input(a number) and returns a statement if a number is prime or not
---
+++ code/primeOrOdd.py
@@ -0,0 +1,21 @@
+def primeOrNot():
+    # Taking User's Input
+    num = int(input("Enter any number: "))
+
+    # Prime number is always greater than 1
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                print(num, "is not a prime number")
+                break
+        else:
+            print(num, "is a prime number")
+
+        '''
+        If the entered number is less than or equal to 1
+        Then it is not prime number
+        '''
+    else:
+        print(num, "is not a prime number")
+
+primeOrNot()
